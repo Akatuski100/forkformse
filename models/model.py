f51ee100047017a7c6bd3499138411fe864bdaa9
Harit Tarwani <112678530+HaritMT25@users.noreply.github.com>
2025-02-25 11:33:56 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -6,7 +6,7 @@ from utils.masking import TriangularCausalMask, ProbMask
 from models.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
 from models.decoder import Decoder, DecoderLayer
 from models.attn import FullAttention, ProbAttention, AttentionLayer
-from models.embed import DataEmbedding, ChannelPositionalEmbedding
+from models.embed import DataEmbedding
 
 class Informer(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
@@ -18,7 +18,7 @@ class Informer(nn.Module):
         self.pred_len = out_len
         self.attn = attn
         self.output_attention = output_attention
-        self.enc_in = enc_in
+
         # Encoding
         # self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         # self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
@@ -26,10 +26,6 @@ class Informer(nn.Module):
         self.dec_embedding = DataEmbedding(c_in=enc_in, d_model=d_model, m=1, embed_type=embed, freq=freq, dropout=dropout)
         # Attention
         Attn = ProbAttention if attn=='prob' else FullAttention
-
-        # Use the channel encoding dimension from the embedding
-        channel_encoding_dim = self.enc_embedding.channel_encoding_dim
-        
         # Encoder
         self.encoder = Encoder(
             [
@@ -39,8 +35,7 @@ class Informer(nn.Module):
                     d_model,
                     d_ff,
                     dropout=dropout,
-                    activation=activation,
-                    channel_encoding_dim=channel_encoding_dim
+                    activation=activation
                 ) for l in range(e_layers)
             ],
             [
@@ -73,14 +68,8 @@ class Informer(nn.Module):
         
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
-        # In Informer.forward:
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
-        # Compute channel encoding (from DataEmbedding parameters):
-        channel_encoder = ChannelPositionalEmbedding(self.enc_in, m=self.enc_embedding.m).to(x_enc.device)
-        channel_encoding = channel_encoder(x_enc.shape[1])
-        channel_encoding = channel_encoding.unsqueeze(0).expand(x_enc.size(0), -1, -1)
-        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask, channel_encoding=channel_encoding)
-
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
@@ -92,6 +81,7 @@ class Informer(nn.Module):
             return dec_out[:,-self.pred_len:,:], attns
         else:
             return dec_out[:,-self.pred_len:,:] # [B, L, D]
+
 
 class InformerStack(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
